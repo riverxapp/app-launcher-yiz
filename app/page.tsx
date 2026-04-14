29feaefa88d6a94dc0fdb9da4766f32777ed074e
Chirag Dodiya <hi@chirag.co>
2026-04-14 16:01:51 +0530

    chore: deterministic patch step 1/1 - app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from "@/components/Navbar";
-import { HeroSection } from "@/components/HeroSection";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
