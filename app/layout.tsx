a0966bcc3b352c389c9e82d6a3da0699255c2754
Chirag Dodiya <hi@chirag.co>
2026-04-14 17:01:55 +0530

    chore: deterministic bootstrap step 1/1 - app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
-          enableSystem
+          enableSystem={true}
           disableTransitionOnChange
         >
           {children}
